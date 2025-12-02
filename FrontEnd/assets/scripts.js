a9bb67b8e25fd5d66a5e2b6b0a04b48cc4ce044e
Colin Cyril <cyrilco18@gmail.com>
2025-12-02 16:10:46 +0100

    init projet

---
+++ FrontEnd/assets/scripts.js
@@ -0,0 +1,106 @@
+
+
+
+/* Call API */
+
+const { response } = require("express");
+
+fetch("http://localhost:5678/api/works")
+  .then(response => response.json())
+  .then(works => {
+    works.forEach(work => {
+      /* Balise creation */
+      let newFigure = document.createElement("figure");
+      let newImg = document.createElement("img");
+      let newCaption = document.createElement("figcaption");
+      let category = work.category.id; /* create category from database. Hidden */
+      /* fill balise */
+      newImg.src = work.imageUrl;
+      newCaption.textContent = work.title;
+      /* assemblage */
+      newFigure.appendChild(newImg);
+      newFigure.appendChild(newCaption);
+       newFigure.dataset.category = category; /* taking category from database */
+      /* fill gallery */
+
+      let gallery = document.querySelector("#gallery");
+      gallery.appendChild(newFigure);
+
+  console.log(category);
+    });
+
+    /*
+    <div id="gallery" class="gallery">
+    <figure>
+    <img src="">
+    <figcaption>
+    </figcaption>
+    </figure>
+    </div>
+    */
+
+
+
+  })
+
+
+
+
+const buttonsFilter = document.querySelectorAll(".projet-buttons");
+const galleryItems = document.querySelectorAll("#gallery figure")
+/*      Si clic button
+      Alors montre button id */
+buttonsFilter.forEach(button => {
+  button.addEventListener("click", () => {
+    buttonsFilter.forEach((button) => button.classList.remove("active"));
+    button.classList.add("active");
+
+    const filterId = button.id;
+    console.log(filterId);
+  
+
+
+/*  pour chaque button clic
+affiche image lié à l'id.*/
+
+
+
+    galleryItems.forEach(item => {
+            const itemCategory = item.dataset.category.id;
+      if (filterId === "all" || filterId === itemCategory) {
+        item.value.remove("hidden");
+      } else { item.value.add("hidden"); };
+    })
+
+  });
+
+
+});
+
+
+
+
+
+/*       
+     "category": {
+      "id": 1,
+      "name": "Objets"
+    } 
+    "category": {
+      "id": 2,
+      "name": "Appartements"}
+    "category": {
+      "id": 3,
+      "name": "Hotels & restaurants"} */
+
+
+
+
+window.addEventListener("load", (event) => {
+  async function fetchProducts() {
+    const response = await fetch("http://localhost:5678/api/works");
+    const products = await response.json(); 
+    return products
+  
+  console.log("page chargée")}
+});
