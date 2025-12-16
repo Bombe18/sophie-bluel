e76096a2eae66c0efe39aaeee85c432e0cb4258b
Colin Cyril <cyrilco18@gmail.com>
2025-12-16 15:11:00 +0100

    Functionnal filter button

--- FrontEnd/assets/scripts.js
+++ FrontEnd/assets/scripts.js
@@ -1,71 +1,65 @@
+window.addEventListener("load", async () => {
+  const buttonsFilter = document.querySelectorAll(".projet-buttons")
+  const gallery = document.querySelector("#gallery")
 
-const buttonsFilter = document.querySelectorAll(".projet-buttons");
-const galleryItems = document.querySelectorAll("#gallery figure");
+  const response = await fetch("http://localhost:5678/api/works")
+  const works = await response.json();
 
-/* Call API */
+  works.forEach(work => {
 
-fetch("http://localhost:5678/api/works")
-  .then(response => response.json())
-  .then(works => {
-    works.forEach(work => {
-      /* Balise creation */
-      let newFigure = document.createElement("figure");
-      let newImg = document.createElement("img");
-      let newCaption = document.createElement("figcaption");
-      let category = work.category.id; /* create category from database. Hidden */
-      /* fill balise */
-      newImg.src = work.imageUrl;
-      newCaption.textContent = work.title;
-      /* assemblage */
-      newFigure.appendChild(newImg);
-      newFigure.appendChild(newCaption);
-      newFigure.dataset.category = category; /* taking category from database */
-      /* fill gallery */
+    /* Balise creation */
+    let newFigure = document.createElement("figure")
+    let newImg = document.createElement("img")
+    let newCaption = document.createElement("figcaption")
+    let category = work.category.id; /* create category from database. Hidden */
+    /* fill balise */
+    newImg.src = work.imageUrl
+    newCaption.textContent = work.title
+    /* assemblage */
+    newFigure.appendChild(newImg)
+    newFigure.appendChild(newCaption)
+    newFigure.dataset.category = category; /* taking category from database */
+    /* fill gallery */
 
-      let gallery = document.querySelector("#gallery");
-      gallery.appendChild(newFigure);
+    let gallery = document.querySelector("#gallery")
+    gallery.appendChild(newFigure)
+  })
 
-      console.log(category);
+   buttonsFilter.forEach(button => {
+    button.addEventListener("click", () => {
+      buttonsFilter.forEach(button => button.classList.remove("active"))
+      button.classList.add("active")
+
+      const filterId = button.id
+
+      const figures = gallery.querySelectorAll("figure")
+
+      figures.forEach(figure => {
+        const figureCategory = figure.dataset.category
+
+        if (filterId === "all" ||  filterId === figureCategory) {
+          figure.classList.remove("hidden")
+        } else {
+          figure.classList.add("hidden")
+        }
+      })
     })
-    
-  });
-
-/*  Si clic button - Alors montre button id */
-buttonsFilter.forEach(button => {
-  button.addEventListener("click", () => {
-    buttonsFilter.forEach((button) => button.classList.remove("active"));
-    button.classList.add("active");
-
-    const filterId = button.id;
-    console.log(filterId);
-
-    /*  pour chaque button clic
-    affiche image lié à l'id.*/
-
-    work.forEach(item => {
-      const work = item.dataset.category.id;
-      if (filterId === "all" || filterId === itemCategory) {
-        item.value.remove("hidden");
-      } else { item.value.add("hidden"); };
-    })
-  });
-});
+  })
+})
 
 
 
 
 
-/*
 
 
-window.addEventListener("load", (event) => {
-  async function fetchProducts() {
-    const response = await fetch("http://localhost:5678/api/works");
-    const products = await response.json(); 
-    return products
-  
-  console.log("page chargée")}
-});
 
 
-*/
+
+
+
+
+
+
+
+
