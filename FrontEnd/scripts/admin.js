636809db0d04ba9a34dc8dabfce5e2b4d7ebe4d3
Colin Cyril <cyrilco18@gmail.com>
2026-01-02 15:39:02 +0100

    functionnal removeCookies

---
+++ FrontEnd/scripts/admin.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", () => {
+    /*Si le token et l'userID est ok
+    Alors, charge le mode admin
+    else -> ne rien faire
+    */
+    const token = localStorage.getItem("authToken");
+    const userId = localStorage.getItem("userId");
+    const loginbutton = document.querySelector("#login");
+    console.log(token, "\n", userId)
+
+    if (token && userId) {
+        changeLoginToLogout();
+        removeCookies();
+    }
+
+    function removeCookies() {
+        loginbutton.addEventListener("click", (event) => {
+            window.localStorage.removeItem("authToken");
+            window.localStorage.removeItem("userId");
+            window.location.reload();
+        })}
+
+        function changeLoginToLogout() {
+            loginbutton.textContent = "logout"
+        }
+
+        function Logout() {
+            //TODO
+        };
+
+        function editMode() {
+            //TODO 
+        };
+
+        function changeUi() {
+            //TODO 
+        };
+    })
