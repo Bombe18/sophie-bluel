11b9765bb4220aaba5891f817ad7c150e59a4d7a
Colin Cyril <cyrilco18@gmail.com>
2026-01-06 14:21:09 +0100

    Add remove filters + edit mode + blackbanner

--- FrontEnd/scripts/admin.js
+++ FrontEnd/scripts/admin.js
@@ -1,16 +1,15 @@
 window.addEventListener("load", () => {
-    /*Si le token et l'userID est ok
-    Alors, charge le mode admin
-    else -> ne rien faire
-    */
     const token = localStorage.getItem("authToken");
     const userId = localStorage.getItem("userId");
-    const loginbutton = document.querySelector("#login");
+    const loginbutton = document.getElementById("login");
     console.log(token, "\n", userId)
 
     if (token && userId) {
         changeLoginToLogout();
         removeCookies();
+        addBlackBanner();
+        removeFilters();
+        addEditMode();
     }
 
     function removeCookies() {
@@ -18,17 +17,45 @@ window.addEventListener("load", () => {
             window.localStorage.removeItem("authToken");
             window.localStorage.removeItem("userId");
             window.location.reload();
-        })}
+        })
+    }
 
-        function changeLoginToLogout() {
-            loginbutton.textContent = "logout"
-        }
+    function changeLoginToLogout() {
+        loginbutton.textContent = "logout"
+    }
 
-        function editMode() {
-            //TODO 
-        };
+    function editMode() {
+        //TODO 
+    };
 
-        function changeUi() {
-            //TODO 
-        };
-    })
+    function addBlackBanner() {
+        let blackBanner = document.createElement("div");
+        blackBanner.id = "blackBanner";
+        blackBanner.innerHTML = `
+        <p class="editMode">
+        <i class="fa-regular fa-pen-to-square"></i> Mode édition
+        </p>
+        `;
+        document.body.insertBefore(blackBanner, document.body.firstChild);
+    };
+
+    function removeFilters() {
+        document.getElementById("list-filters").hidden = true;
+    };
+
+    function addEditMode() {
+        let editMode = document.createElement("p");
+        let insert = document.getElementsByClassName("titleh2")[0]
+
+        editMode.innerHTML = `
+        <i class="fa-regular fa-pen-to-square"></i> 
+        modifier
+        `;
+        insert.after(editMode)
+    }
+
+    function windowModale () {
+        
+    }
+
+})
